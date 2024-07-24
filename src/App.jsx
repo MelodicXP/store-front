e2e2878495785b9f6a313a4596e6bd8b894f7681
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-07-24 12:30:36 -0500

    Ability to increment and decrement counter

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,27 @@ import { decrement, increment } from './store/counter.js';
 function App() {
 
   // Pulls state via useSelector from counter.js in the intiial state
-  const count = useSelector((state) => state.count);
+  const count = useSelector((state) => state.counter.count);
+
+  // Use to dispatch actions
+  const dispatch = useDispatch();
+
+  // On click dispatch action
+  const handleDecrement = () => {
+    dispatch(decrement(1));
+  }
+
+  // On click dispatch action
+  const handleIncrement = () => {
+    dispatch(increment(1));
+  }
   
   return (
     <>
       <h1>Redux Counter</h1>
+      <h3>Current Count: {count}</h3>
+      <button onClick={handleDecrement}>Decrement</button>
+      <button onClick={handleIncrement}>Increment</button>
     </>
   )
 }
