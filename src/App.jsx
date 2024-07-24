099698fe09c7c862d594e929304fa35ed7b09a8f
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-07-24 12:22:48 -0500

    Import actions from store

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
-import './App.css'
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from './store/counter.js';
 
 function App() {
+
+  // Pulls state via useSelector from counter.js in the intiial state
+  const count = useSelector((state) => state.count);
+  
   return (
     <>
       <h1>Redux Counter</h1>
