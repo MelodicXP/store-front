4d7c770e84f34f4850eb985d94a5b938b8e160df
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-07-24 12:17:44 -0500

    Refactor to fix bug

--- src/store/index.js
+++ src/store/index.js
@@ -3,14 +3,11 @@
 /* This format used every time setting up a new file */
 import { createStore, combineReducers } from 'redux';
 import { composeWithDevTools } from '@redux-devtools/extension';
-import { counter } from './counter';
-
+import counter from './counter.js'; // Name as you please'counter' 'counterReducer', etc...
 
 let reducers = combineReducers({ counter });
 
-const store = () => {
-  return createStore(reducers, composeWithDevTools());
-}
+const store = createStore(reducers, composeWithDevTools());
 
 export default store;
 
