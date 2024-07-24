ea712a784a896c34a6143510a39b90a5089a7dbe
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-07-24 16:25:27 -0500

    Initial commit

---
+++ src/Components/Categories/index.jsx
@@ -0,0 +1,14 @@
+'use strict';
+
+const Categories = () => {
+  return (
+    <div>
+     <ul>
+      <li>Electronics</li>
+      <li>Food</li>
+     </ul>
+    </div>
+  );
+}
+
+export default Categories;
