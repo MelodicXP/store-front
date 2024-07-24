df49132c1aa011c962b225bda885a5407412684d
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-07-24 16:25:41 -0500

    Initial commit

---
+++ src/Components/Products/index.jsx
@@ -0,0 +1,14 @@
+'use strict';
+
+const Products = () => {
+  return (
+    <div>
+      <ul>
+        <li>Test Electronics item</li>
+        <li>Test 2 Electronics item</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Products;
