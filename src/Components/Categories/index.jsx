74188e0b9d59ff692c6d1d16bd0e56cc58eabd0e
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-07-25 14:09:27 -0500

    Refactor

--- src/Components/Categories/index.jsx
+++ src/Components/Categories/index.jsx
@@ -1,27 +1,20 @@
 'use strict';
 
-import { useSelector, useDispatch } from "react-redux";
-// import { decrement, increment } from './store/counter.js';
-
-const handleDisplayElectronics = () => {
-  console.log('Handle Display Electronics');
-}
-
-const handleDisplayFood = () => {
-  console.log('Handle Display Food');
-}
-
 const Categories = () => {
+  const handleDisplayElectronics = () => {
+    console.log('Handle Display Electronics');
+  }
+  
+  const handleDisplayFood = () => {
+    console.log('Handle Display Food');
+  }
+
   return (
     <div>
-     <ul>
       <button onClick={handleDisplayElectronics}>Electronics</button>
       <button onClick={handleDisplayFood}>Food</button>
-      <li>Electronics</li>
-      <li>Food</li>
-     </ul>
     </div>
   );
 }
 
-export default Categories;``
+export default Categories;
