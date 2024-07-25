293c3ac49809559b82e7f58fcc9b994ede27409e
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-07-25 15:31:43 -0500

    ARender category selected with category description

--- src/Components/Categories/index.jsx
+++ src/Components/Categories/index.jsx
@@ -1,23 +1,40 @@
 'use strict';
-import { useDispatch } from "react-redux";
-import { showChosenCategory } from "../../store/counter";
+import { useDispatch, useSelector } from "react-redux";
+import { showChosenCategory, showCategoryDescription } from "../../store/counter";
 
 const Categories = () => {
-
   const dispatch = useDispatch();
+
+  const category = useSelector((state) => state.counter.filteredCategory);
 
   const handleDisplayElectronics = () => {
     dispatch(showChosenCategory('ELECTRONICS'));
+    dispatch(showCategoryDescription('ELECTRONICS'));
   }
   
   const handleDisplayFood = () => {
     dispatch(showChosenCategory('FOOD'));
+    dispatch(showCategoryDescription('FOOD'));
   }
 
   return (
     <div>
-      <button onClick={handleDisplayElectronics}>Electronics</button>
-      <button onClick={handleDisplayFood}>Food</button>
+      <p>Browse our Categories</p>
+      <span 
+        onClick={handleDisplayElectronics} 
+        style={{ cursor: 'pointer', color: 'blue', textDecoration: 'underline', marginRight: '10px' }}
+      >
+        Electronics
+      </span>
+      <span 
+        onClick={handleDisplayFood} 
+        style={{ cursor: 'pointer', color: 'blue', textDecoration: 'underline' }}
+      >
+        Food
+      </span>
+      {category && <h3>{category.category}</h3>}
+      {category && <h4>{category.description}</h4>}
+
     </div>
   );
 }
