f306dc8fe5ac33dc0faa9f5fb08c7a9e464d33b9
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-07-25 14:52:25 -0500

    Display items based on filter

--- src/Components/Products/index.jsx
+++ src/Components/Products/index.jsx
@@ -1,13 +1,9 @@
 'use strict';
 import { useSelector } from "react-redux";
-// import { decrement, increment } from './store/counter.js';
 
 const Products = () => {
   // Pulls state via useSelector from counter.js in the intiial state
-  const products = useSelector((state) => state.counter.products);
-
-  // Use to dispatch actions
-  // const dispatch = useDispatch();
+  const products = useSelector((state) => state.counter.filteredProducts);
   
   return (
     <div>
