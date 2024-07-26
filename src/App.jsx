f724876ad0c1901dbb8e1b8115754850721dbca4
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-07-26 12:41:05 -0500

    Refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,14 @@
-import { useSelector, useDispatch } from "react-redux";
-import { decrement, increment } from './store/counter.js';
 import Header from './Components/Header/index.jsx'
 import Footer from './Components/Footer/index.jsx';
 import Categories from "./Components/Categories/index.jsx";
 import Products from "./Components/Products/index.jsx";
 
-function App() {
-
-  // Pulls state via useSelector from counter.js in the intiial state
-  const count = useSelector((state) => state.counter.count);
-  const numberOfClicks = useSelector((state) => state.counter.numberOfClicks);
-
-  // Use to dispatch actions
-  const dispatch = useDispatch();
-
-  // On click dispatch action
-  const handleDecrement = () => {
-    dispatch(decrement(1));
-  }
-
-  // On click dispatch action
-  const handleIncrement = () => {
-    dispatch(increment(1));
-  }
-  
+function App() { 
   return (
     <>
       <Header/>
       <Categories/>
       <Products />
-      <h3>Current Count({numberOfClicks}): {count}</h3>
-      <button onClick={handleDecrement}>Decrement</button>
-      <button onClick={handleIncrement}>Increment</button>
       <Footer/>
     </>
   )
