928c31ec134225546f64a09854b6e0e20c521e35
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-07-29 11:37:32 -0500

    Refactor to use combined reducer

--- src/store/index.js
+++ src/store/index.js
@@ -3,8 +3,8 @@
 /* This format used every time setting up a new file */
 import { createStore, combineReducers } from 'redux';
 import { composeWithDevTools } from '@redux-devtools/extension';
-import categories from './counter.js';
-import products from './products.js' // Name as you please'counter' 'counterReducer', etc...
+import categories from './categories/categories.js';
+import products from './products/products.js' // Name as you please'counter' 'counterReducer', etc...
 
 let reducers = combineReducers({ categories, products });
 
